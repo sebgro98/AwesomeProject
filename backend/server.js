eb360eeb2c89910988023c8b3af4d2cf7e5ee978
Liam Battini <liamb@kth.se>
2024-02-05 15:57:19 +0100

    trying to start server ahahaha funny yes

---
+++ backend/server.js
@@ -0,0 +1,33 @@
+const path = require('path');
+const APP_ROOT_DIR = __dirname;
+
+require('dotenv-safe').config({
+    path: path.join(APP_ROOT_DIR, '.env'),
+});
+
+const express = require('express');
+const app = express();
+
+const bodyParser = require('body-parser');
+app.use(bodyParser.json());
+
+const cookieParser = require('cookie-parser');
+app.use(cookieParser());
+
+app.use(express.static(path.join(APP_ROOT_DIR, 'public')));
+
+app.get('/', (req, res) => {
+    return res.send('hello :)');
+});
+
+const reqHandlerLoader = require('./api');
+//reqHandlerLoader.loadHandlers(app);
+//reqHandlerLoader.loadErrorHandlers(app);
+
+const server = app.listen(
+    process.env.SERVER_PORT,
+    process.env.SERVER_HOST,
+    () => {
+        console.log(`Server on port ${server.address().address}:${server.address().port}`);
+    }
+);
