e690e07820a10ba833825d96fc75298f7d318661
Sushil's pc <sushilkc955@outlook.com>
2024-02-06 14:20:04 +0100

    Added some comments in Api

--- backend/src/api/error/ErrorHandler.js
+++ backend/src/api/error/ErrorHandler.js
@@ -1,11 +1,18 @@
 const UserController = require('../../controller/Controller');
-
+/**
+ * Base class for handling errors and retrieving the user controller.
+ */
 class ErrorHandler {
 
+    /**
+     * Retrieves the user controller asynchronously and assigns it to the instance variable `contr`.
+     * @throws {Error} Throws an error if there is an issue retrieving the controller.
+     */
     async retrieveController() {
         this.contr = await UserController.createController();
     }
 }
 
+// Export the ErrorHandler class to make it available for other modules
 module.exports = ErrorHandler;
 
--- backend/src/api/error/ErrorResponseSender.js
+++ backend/src/api/error/ErrorResponseSender.js
@@ -1,18 +1,36 @@
 const ErrorHandler = require('./ErrorHandler');
-
+/**
+ * Represents a class responsible for sending error responses.
+ * @extends ErrorHandler
+ */
 class ErrorResponseSender extends ErrorHandler {
+    /**
+     * Creates an instance of ErrorResponseSender.
+     */
     constructor() {
         super();
     }
 
+    /**
+     * Returns the path associated with the ErrorResponseSender.
+     * @returns {string} The path for error response handling.
+     */
     static get ERROR_RESPONSE_SENDER_PATH() {
         return '/'
     }
 
+    /**
+     * Gets the path for the current ErrorResponseSender instance.
+     * @returns {string} The path for the current instance.
+     */
     get path() {
         return ErrorResponseSender.ERROR_RESPONSE_SENDER_PATH;
     }
 
+    /**
+     * Registers the error response handling middleware for the provided Express app.
+     * @param {express.Application} app - The Express app to register the error handling middleware.
+     */
     registerHandler(app) {
         app.use(this.path, (err, req, res, next) => {
             if (res.headersSent) {
@@ -23,4 +41,5 @@ class ErrorResponseSender extends ErrorHandler {
     }
 }
 
+// Export the ErrorResponseSender class to make it available for other modules
 module.exports = ErrorResponseSender;
