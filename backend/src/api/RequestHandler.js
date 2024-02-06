6c92cfff18c0a21d351e03326bff278c39429464
ingem <ingemarc@kth.se>
2024-02-06 14:22:00 +0100

    wrote comments on index.js and RequestHandler.js

--- backend/src/api/RequestHandler.js
+++ backend/src/api/RequestHandler.js
@@ -1,19 +1,44 @@
 const express = require('express');
 const UserController = require('../controller/Controller');
 
+/**
+ * RequestHandler class manages HTTP requests and responses.
+ */
 class RequestHandler {
+
+    /**
+     * Constructs a new RequestHandler.
+     * Initializes an Express router.
+     * @constructor
+     */
     constructor() {
         this.router = express.Router();
     }
 
+    /**
+     * Retrieves the URL prefix used by the RequestHandler.
+     * @static
+     * @returns {string} The URL prefix.
+     */
     static get URL_PREFIX() {
         return 'http://';
     }
 
+    /**
+     * Asynchronously retrieves an instance of the UserController.
+     * @async
+     */
     async retrieveController() {
         this.contr = await UserController.createController();
     }
 
+    /**
+     * Sends an HTTP response with the specified status code and body.
+     * If the body is undefined, only the status code is sent.
+     * @param {Object} res - The Express response object.
+     * @param {number} status - The HTTP status code.
+     * @param {Object} body - The response body.
+     */
     sendHttpResponse(res, status, body) {
         if (body === undefined) {
             res.status(status).end();
--- backend/src/api/index.js
+++ backend/src/api/index.js
@@ -1,21 +1,42 @@
 const PersonAPI = require ('./PersonAPI');
 const ErrorResponseSender = require('./error/ErrorResponseSender')
 
+/**
+ * RequestHandlerLoader class loads request handlers and error handlers into an Express application.
+ * It provides methods to add request handlers and error handlers, and to load them into an Express app.
+ * It also instantiates and adds specific request handlers and error handlers upon creation.
+ */
 class RequestHandlerLoader {
 
+    /**
+     * Creates a new instance of RequestHandlerLoader with empty arrays for request handlers and error handlers.
+     * @constructor
+     */
     constructor() {
         this.reqHandlers = [];
         this.errorHandlers = [];
     }
 
+    /**
+     * Adds a request handler to the loader.
+     * @param {Object} reqHandler The request handler object to add.
+     */
     addRequestHandler(reqHandler) {
         this.reqHandlers.push(reqHandler);
     }
 
+    /**
+     * Adds an error handler to the loader.
+     * @param {Object} errorHandler The error handler object to add.
+     */
     addErrorHandler(errorHandler) {
         this.errorHandlers.push(errorHandler);
     }
 
+    /**
+     * Loads the registered request handlers into the Express application.
+     * @param {Object} app The Express application to load request handlers into.
+     */
     loadReqHandlers(app) {
         this.reqHandlers.forEach((reqHandler) => {
             reqHandler.registerHandler();
@@ -23,6 +44,10 @@ class RequestHandlerLoader {
         })
     }
 
+    /**
+     * Loads the registered error handlers into the Express application.
+     * @param {Object} app The Express application to load error handlers into.
+     */
     loadErrorHandlers(app) {
         this.errorHandlers.forEach((errorHandler) => {
             errorHandler.registerHandler(app);
@@ -30,6 +55,7 @@ class RequestHandlerLoader {
     }
 }
 
+// Instantiate and configure the RequestHandlerLoader with specific request and error handlers
 const loader = new RequestHandlerLoader();
 loader.addRequestHandler(new PersonAPI());
 loader.addErrorHandler(new ErrorResponseSender());
