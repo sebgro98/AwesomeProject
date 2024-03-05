5f00ce05177706dab4e16f7415f2d2f12098ac91
sebgro98 <sebgro@kth.se>
2024-03-05 13:46:45 +0100

    Added Header.js and Footer.js

---
+++ frontend/src/Header.js
@@ -0,0 +1,12 @@
+import React from 'react';
+
+function Footer() {
+    return (
+        <footer>
+            {/* Your footer content here */}
+            <p>© 2024 My App, Inc.</p>
+        </footer>
+    );
+}
+
+export default Footer;
