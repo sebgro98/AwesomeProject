d3c3ad19af34ba155d7ee9cd72e35ded08b3b136
sebgro98 <sebgro@kth.se>
2024-03-05 19:22:39 +0100

    fixed logout HeaderPresenter.js

--- frontend/src/presenter/HeaderPresenter.js
+++ frontend/src/presenter/HeaderPresenter.js
@@ -1,13 +1,12 @@
-// HeaderPresenter.js
+
 import React from 'react';
 import { useNavigate } from 'react-router-dom';
-import HeaderView from '../views/HeaderView'; // Assume this is the path to your view component
+import HeaderView from '../views/HeaderView';
 
 const HeaderPresenter = () => {
     const navigate = useNavigate();
 
     const handleLogout = () => {
-        // Place your logout logic here, for example, clearing user tokens
         navigate('/');
     };
 
